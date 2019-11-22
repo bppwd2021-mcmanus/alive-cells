9516819a69c0b9b74e66f723d72fbd73653b0676
Dexter <N.Dexter@baypath.net>
2019-11-22 14:06:43 -0500

    Nathan's update 11/22/2019

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("hi");
         for (int i = 0; i < 3; i++) {
-            System.out.println("I LOVE KEVIN AND NATHAN. YOU ARE BOTH NICE PEOPLE!");
+            System.out.println("poop");
         }
     }
 }
