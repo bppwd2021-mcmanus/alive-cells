c541e7f6eaecb490557726ee77443d077a001e36
bppwd2021-Consiglio <c.consiglio@baypath.net>
2019-11-22 14:02:09 -0500

    yay

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("hi");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("I LOVE KEVIN AND NATHAN. YOU ARE BOTH NICE PEOPLE!");
+        }
     }
 }
