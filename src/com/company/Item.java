97d397f0a4a00854328e6e0b9ee0aa4bf0e504ee
McManus <K.McManus@baypath.net>
2019-12-05 13:10:19 -0500

    yes

---
+++ src/com/company/Item.java
@@ -0,0 +1,36 @@
+package com.company;
+import java.awt.*;
+public class Item {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int use;
+    private int val;
+    public Item(){
+        x=0;
+        y=0;
+        width=0;
+        height=0;
+        use=0;
+        val=0;
+    }
+    public Item(int x, int y, int w, int h, int u, int v){
+        this.x=x;
+        this.y=y;
+        this.width=w;
+        this.height=h;
+        this.use=u;
+        this.val=v;
+    }
+    public void useItem(){
+        use --;
+        if(use<1){
+            x=0;
+            y=0;
+            width=0;
+            height=0;
+        }
+    }
+}
+
